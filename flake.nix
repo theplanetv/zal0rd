e5667b6eccc28ff89fada8c3d3182adf2205305c
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-10-03 20:02:51 +0700

    Init repo

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "nix flake for zal0rd";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in {
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [
+              # Development tools
+              pkgs.go
+              pkgs.pkgconf
+              pkgs.xorg.libX11
+              pkgs.xorg.libXcursor
+              pkgs.xorg.libXrandr
+              pkgs.xorg.libXinerama
+              pkgs.xorg.libXi
+              pkgs.xorg.libXxf86vm
+              pkgs.libGL
+
+              # Language servers
+              pkgs.gopls
+              pkgs.nixd
+
+              # Others tools
+              pkgs.scc
+            ];
+          };
+        });
+    };
+}
